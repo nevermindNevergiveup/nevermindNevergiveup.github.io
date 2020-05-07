64d9182ce41a087471de3e9fb247b1cf42398030
nevermindNevergiveup <63172280+nevermindNevergiveup@users.noreply.github.com>
2020-05-08 01:51:40 +0530

    fdsfdsf

--- create-file.php
+++ create-file.php
@@ -5,7 +5,7 @@
 
     $json = $_POST['data']; //json need to be data
     $info = json_encode($json);
-    $file = fopen('test.json','w+') or die("File not found");
+    $file = fopen('test.json','a+') or die("File not found");
     fwrite($file, $info);
     fclose($file);exit;
 ?>
