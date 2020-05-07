72225d723551b49ad9b6757ebe93c8fc4d22524a
nevermindNevergiveup <63172280+nevermindNevergiveup@users.noreply.github.com>
2020-05-07 13:44:55 +0530

    dsad3333

--- saveit.php
+++ saveit.php
@@ -12,7 +12,7 @@
     $arr_data = json_decode($jsondata, true);
     $arr_data[] = $formdata;
     $jsondata = json_encode($arr_data, JSON_PRETTY_PRINT);
-    file_put_contents('js/list.json', $jsondata);
+    file_put_contents('list.json', $jsondata);
 
     $form_state['redirect'] = false;
 
