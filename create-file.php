4ceb7d234874b73e1d3e5b90bde5db1082f91390
nevermindNevergiveup <63172280+nevermindNevergiveup@users.noreply.github.com>
2020-05-08 01:07:07 +0530

    fdsfdsf

---
+++ create-file.php
@@ -0,0 +1,11 @@
+<?php
+    //comment these two lines when errors are resolved
+    error_reporting(E_ALL);
+    ini_set('display_errors',1);
+
+    $json = $_POST['data']; //json need to be data
+    $info = json_encode($json);
+    $file = fopen('test.json','w+') or die("File not found");
+    fwrite($file, $info);
+    fclose($file);exit;
+?>
